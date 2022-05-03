628eb19c5415b65dc9d78e868f18f03ccff1c64c
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2022-05-04 00:11:09 +0200

    Add button text updating on toggling the form

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const signUpBtn = document.querySelector('.green-bg button');
 
 signUpBtn.addEventListener('click', () => {
 	container.classList.toggle('change');
+	container.classList.contains('change')
+		? (signUpBtn.innerHTML =
+				'<i class="fa-solid fa-circle-xmark"></i> Close form')
+		: (signUpBtn.innerHTML =
+				'<i class="fa-solid fa-arrow-right-to-bracket"></i> Sign Up');
 });
